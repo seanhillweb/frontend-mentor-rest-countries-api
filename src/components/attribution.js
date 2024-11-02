7de171fbb5e951032a2f77b708f0cf17b7f7806c
seanhillweb <seanhillweb@gmail.com>
2024-11-01 21:17:35 -0500

    feat: add project attribution component

---
+++ src/components/attribution.js
@@ -0,0 +1,29 @@
+/**
+ * Summary. Attribution for Frontend Mentor and Sean Hill.
+ */
+
+export default function Attribution() {
+  return (
+    <footer
+      className="px-4 pb-12 pt-3 text-center text-sm text-gray-700 dark:text-gray-300"
+      aria-label="Site"
+    >
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        className="text-brand-dark-blue underline hover:no-underline focus:no-underline dark:text-white"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        href="https://www.seanhillweb.com"
+        className="text-brand-dark-blue underline hover:no-underline focus:no-underline dark:text-white"
+      >
+        Sean Hill
+      </a>
+      .
+    </footer>
+  );
+}
